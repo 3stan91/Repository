1f4eb77fa3f7ff5873fa6513154429dd96bfb717
Alwxandr <3stan91@gmail.com>
2023-01-15 09:42:29 +0600

    minutes

--- trainSchedule/main.cpp
+++ trainSchedule/main.cpp
@@ -1,103 +1,113 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
 
 using namespace std;
-void convertValues(string time, int& hour, int& minute);
-bool isCheckedData(string time);
+bool convertStrToInt(string, int&, int&);
 
-int main(){
+/*void IsCheckedData(string time)
+ {
+ unsigned int i=0;
+ bool error = false;
+
+ while (!error && i < time.length())
+ {
+ if(time[0]>2 || (time[i]==':' && time[i+1]>5) || (time[i] <= '0' && time[i] >= '9') )
+ {
+ cout << "Ошибка! Вы ввели неверный формат времени.\n";
+ error=true;
+ }
+ else if(time[1]==':')
+ time='0'+time;
+ else
+ i++;
+ }
+ }
+ */
+
+int main() {
 	setlocale(LC_ALL, "ru");
 	const int MIN60 = 60;
 	string timeDeparture, timeArrival;
 	cout << "Введите время отправления в формате (ЧЧ:MM)\n";
-	getline(cin, timeDeparture);
+//	getline(cin, timeDeparture);
 	cout << "Введите время прибытия в формате (ЧЧ:MM)\n";
-	getline(cin, timeArrival);
+//	getline(cin, timeArrival);
 
-	int hourDeparture=0, minuteDeparture=0;
-	int hourArrival =0, minuteArrival=0;
+	timeDeparture = "23:25";
+	timeArrival = "14:55";
+	int hourDeparture = 0, minuteDeparture = 0;
+	int hourArrival = 0, minuteArrival = 0;
 
-	if(isCheckedData(timeDeparture))
-		convertValues(timeDeparture, hourDeparture, minuteDeparture);
+	if (!convertStrToInt(timeDeparture, hourDeparture, minuteDeparture))
+		return -1;
 
-	if(isCheckedData(timeArrival))
-		convertValues(timeArrival, hourArrival, minuteArrival);
+//	isCheckedData(timeArrival);
+	if (!convertStrToInt(timeArrival, hourArrival, minuteArrival))
+		return -1;
 
 	int hoursTrip, minutesTrip;
-	//int min = 60 - minuteDeparture;
+	int min = 60 - minuteDeparture;
 
-	if((minuteArrival + MIN60 - minuteDeparture)>= MIN60)
-		{
-		   minutesTrip=(minuteArrival + MIN60 - minuteDeparture) % MIN60;
-		}
+	if (minuteArrival >= minuteDeparture)
+		minutesTrip = minuteArrival - minuteDeparture;
+	else if (minuteArrival < minuteDeparture) {
+		hourArrival -= 1;
+		minutesTrip = minuteArrival + min;
+	}
+	//else		minutesTrip = min + minuteArrival;
 
-	if(hourArrival == hourDeparture && minuteArrival == minuteDeparture)
-	{
+	if (hourArrival == hourDeparture && minuteArrival == minuteDeparture) {
 		hoursTrip = 24;
 		minutesTrip = 0;
-	}
-	else if(hourArrival < hourDeparture)
+	} else if (hourArrival < hourDeparture)
 		hoursTrip = abs(24 - hourDeparture) + hourArrival;
 	else
-		hoursTrip=hourArrival-hourDeparture;
+		hoursTrip = hourArrival - hourDeparture - 1;
 
-	cout << "Поездка составила " << hoursTrip <<" часа "<< minutesTrip << " мин \n";
+	cout << "Поездка составила " << hoursTrip << " часа " << minutesTrip
+			<< " мин \n";
 	return 0;
 }
 
-void convertValues(string time, int& hour, int& minute)
-{
+bool convertStrToInt(string time, int &hour, int &minute) {
 	unsigned int i = 0;
-
-	while (i < time.length())
-		{
-			//if (time[i] <= '0' && time[i] >= '9'&& time[i] != ':')
-				//cout << "Ошибка!";
-
-			if(time[i]!=':')
-			{
-				switch(i)
-				{
-				 case 0:
-					hour = 10 * ((int)time[i] - (int)'0');
-					break;
-				 case 1:
-					hour = hour + ((int)time[i] - (int)'0');
-					break;
-				 case 3:
-					minute = 10* ((int)time[i] - (int)'0');
-					break;
-				 case 4:
-					minute = minute + ((int)time[i] - (int)'0');
-					break;
-				}
-			}
-			else
-			{
-				i++;
-				continue;
-			}
-		    i++;
-		}
-	return;
-}
-
-bool IsCheckedData(string t)
-{
-	unsigned int i=0;
 	bool error = false;
-	while (i < t.length())
-	{
-		if (t[i] <= '0' && t[i] >= '9' && t[i] != ':')
-		{
+
+	while (i < time.length()) {
+		if ((time[0] >= '2' && time[1] >= '4')
+				|| (time[i] == ':' && time[i + 1] > '5')
+				|| (time[i] <= '0' && time[i] >= '9')) {
 			cout << "Ошибка! Вы ввели неверный формат времени.\n";
-			error=true;
-		}
-		else
+			error = true;
+			break;
+		} else if (time[1] == ':')
+			time = '0' + time;
+
+		if (time[i] != ':') {
+			switch (i) {
+			case 0:
+				hour = 10 * ((int) time[i] - (int) '0');
+				break;
+			case 1:
+				hour = hour + ((int) time[i] - (int) '0');
+				break;
+			case 3:
+				minute = 10 * ((int) time[i] - (int) '0');
+				break;
+			case 4:
+				minute = minute + ((int) time[i] - (int) '0');
+				break;
+			}
+		} else {
 			i++;
+			continue;
+		}
+		i++;
 	}
-	if(!error)
+	if (!error)
 		return true;
 	else
 		return false;
 }
+
